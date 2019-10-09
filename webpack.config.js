9ad7612597aa51fdb82d5484a80cce9a034e6723
hchangjae <hcj@payw.org>
2019-10-09 20:18:47 +0900

    fix: fix detail of webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 var path = require('path')
 
 module.exports = {
-    entry: './src/app/main.js',
+    target: "node",
+    entry: {
+        app:['./src/app/main.js']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+    externals: [nodeExternals()]
 };
