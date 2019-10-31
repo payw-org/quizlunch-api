45111073583737a903ce4c91bbb07bf5db2f4dbc
wseungjin <bose1021@naver.com>
2019-10-31 22:27:44 +0900

    fixing config

--- src/app/configs/environments.js
+++ src/app/configs/environments.js
@@ -2,10 +2,10 @@
 
 const environments = {
     development: {
-      host: process.env.MYSQL.host,
-      user: process.env.MYSQL.user,
-      password: process.env.MYSQL.password,
-      database: process.env.MYSQL.password
+      host: process.env.MYSQL_host,
+      user: process.env.MYSQL_user,
+      password: process.env.MYSQL_password,
+      database: process.env.MYSQL_database
     }
   }
 
