ec4312fc3448b64c66354a08f9421e79ef36c175
wseungjin <bose1021@naver.com>
2019-11-01 20:02:50 +0900

    update

--- src/app/controllers/commentController.js
+++ src/app/controllers/commentController.js
@@ -37,7 +37,7 @@ exports.delete = async (req, res) => {
 
       const connection = await DBConnector.getConnection()
 
-      const [result] = await connection.query("DELETE FROM comments WHERE commentID=(SELECT commentID from comments where commentID='"+req.body.commentID+"' and password='"+req.body.password+"')", function(err,  rows, fields) {
+      const [result] = await connection.query("DELETE FROM comments WHERE commentID in (SELECT commentID from comments where commentID='"+req.body.commentID+"' and password='"+req.body.password+"')", function(err,  rows, fields) {
       if (!err)
       {
         res.send(result)
