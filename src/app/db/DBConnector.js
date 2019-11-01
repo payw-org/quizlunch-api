b8e7a7df3189bdb5272e32827f442ac5f9544bc2
hchangjae <hcj@payw.org>
2019-11-01 11:43:34 +0900

    chore: remove comments

--- src/app/db/DBConnector.js
+++ src/app/db/DBConnector.js
@@ -13,22 +13,3 @@ class DBConnector {
 }
 
 module.exports = DBConnector
-
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : config.user,
-//   password : config.password,
-//   database : config.database
-// });
-// exports.connect = function(callback){
-//     connection.connect(function(err) {
-//         if (err) {
-//             console.error('mysql connection error');
-//             console.error(err);
-//             throw err;
-//         }
-//         console.log("connected db")
-//         callback();
-//       });    
-// }
-// exports.connector = connection;
