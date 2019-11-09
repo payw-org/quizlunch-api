0840dbfc4ec1d817669cc77ca4536a7192b4b78e
wseungjin <bose1021@naver.com>
2019-11-09 20:33:54 +0900

    debugging

--- src/app/routes/comment.js
+++ src/app/routes/comment.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const commentController = require('../controllers/commentController');
 
-router.get('/:quizID', commentController.getOneQuizComments);
+// router.get('/:quizID', commentController.getOneQuizComments);
 
 router.post('/:quizID', commentController.create);
 
--- src/app/routes/quiz.js
+++ src/app/routes/quiz.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const quizController = require('../controllers/quizController');
 
-router.get('/:quizID', quizController.get);
+// router.get('/:quizID', quizController.get);
 
 router.get('/:quizID/:answer', quizController.correctCheck);
 
