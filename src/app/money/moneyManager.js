267936511a46433a3da76c3d21c7adec8d2771fd
wseungjin <bose1021@naver.com>
2019-11-13 21:46:01 +0900

    testing

--- src/app/money/moneyManager.js
+++ src/app/money/moneyManager.js
@@ -10,12 +10,14 @@ module.exports =  class MoneyManager {
     const result = await DBConnector.getQuiz(quizID)
     var quizTime = new Date(result.time)
 
-    setInterval(updateMoney, 60000);
+    setInterval(updateMoney, 600);
     function updateMoney() {
 
       var nowTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"});
       nowTime = new Date(nowTime);
-      var timeMoney=((nowTime.getHours() * 60  + nowTime.getMinutes() * 1 ) - (quizTime.getHours() * 60  + quizTime.getMinutes()*1))*2
+      // var timeMoney=((nowTime.getHours() * 60  + nowTime.getMinutes() * 1 ) - (quizTime.getHours() * 60  + quizTime.getMinutes()*1))*2
+      var timeMoney=((nowTime.getMinutes() * 60  + nowTime.getSeconds() * 1 ) - (quizTime.getMinutes() * 60  + quizTime.getSeconds()*1))*1
+
       if(timeMoney<0)
       {
         timeMoney=0
