2a3c050911a89a1e56325d3371c198d39989ec6c
wseungjin <bose1021@naver.com>
2019-11-13 21:59:36 +0900

    modularization time function

--- src/app/money/moneyManager.js
+++ src/app/money/moneyManager.js
@@ -2,22 +2,34 @@ const DBConnector = require('../db/DBConnector');
 const WSConnector = require('../websocket/WSConnector');
 
 module.exports =  class MoneyManager {
+
+  static async getQuizTime()
+  {
+    const quizID = await DBConnector.getLastestQuizID()
+    const result = await DBConnector.getQuiz(quizID)
+    var quizTime = new Date(result.time)
+    return quizTime
+  }
+
+  static async getNowTime()
+  {
+    var nowTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"});
+    nowTime = new Date(nowTime);
+    return nowTime;
+  }
+  
   
   static async getMoney(){
     const defaultMoney=1000;
     var nowMoney;
-    const quizID = await DBConnector.getLastestQuizID()
-    const result = await DBConnector.getQuiz(quizID)
-    var quizTime = new Date(result.time)
+    const quizTime = this.getQuizTime()
 
     setInterval(updateMoney, 600);
     function updateMoney() {
 
-      var nowTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"});
-      nowTime = new Date(nowTime);
+      var nowTime = this.getNowTime()
       // var timeMoney=((nowTime.getHours() * 60  + nowTime.getMinutes() * 1 ) - (quizTime.getHours() * 60  + quizTime.getMinutes()*1))*2
       var timeMoney=((nowTime.getHours() * 3600 +nowTime.getMinutes() * 60  + nowTime.getSeconds() * 1 ) - (quizTime.getHours()* 3600 +quizTime.getMinutes() * 60  + quizTime.getSeconds()*1))*1
-
       if(timeMoney<0)
       {
         timeMoney=0
