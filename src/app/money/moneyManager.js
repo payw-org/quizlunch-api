e58cb78359715149073bae6665a15a9f2b972417
wseungjin <bose1021@naver.com>
2019-11-14 17:26:37 +0900

    testing

--- src/app/money/moneyManager.js
+++ src/app/money/moneyManager.js
@@ -4,36 +4,37 @@ const WSConnector = require('../websocket/WSConnector');
 module.exports =  class MoneyManager {
 
 
-  // static async isItSolved()
-  // {
-  //   const result = await DBConnector.getLatestQuiz()
-  // //   var gotAnswer = result.gotAnswer
-  // //   return gotAnswer
-  // // }
+  static async isItSolved()
+  {
+    const result = await DBConnector.getLatestQuiz()
+    var gotAnswer = result.gotAnswer
+    return gotAnswer
+  }
 
-  // static async getLatestQuizTime()
-  // {
-    
-  // }
+  static async getLatestQuizTime()
+  {
+    var quiz = await DBConnector.getLatestQuiz()
+    var quizTime = new Date(quiz.time)
+    return quizTime
+  }
 
-  // static async getNowTime()
-  // {
-    
-  //   return nowTime;
-  // }
-  
-  
+  static async getNowTime()
+  {
+    var nowTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"});
+    nowTime = new Date(nowTime);
+  }
+
+
   static async getMoney(){
     const defaultMoney=1000;
+
     setInterval(updateMoney, 6000);
     async function updateMoney() {
 
-      var quiz = await DBConnector.getLatestQuiz()
-      if(quiz.gotAnswer==0)// If no one send correct answer
+      if(this.isItSolved()==0)// If no one send correct answer
       { 
-        var quizTime = new Date(quiz.time)
-        var nowTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"});
-        nowTime = new Date(nowTime);
+        var quizTime = this.getLatestQuizTime()
+        var nowTime =  this.getNowTime()
         var timeMoney=0
         if(nowTime.getDate()!=quizTime.getDate())
         {
@@ -54,4 +55,39 @@ module.exports =  class MoneyManager {
 
   
 }
+
+
+  
+  
+//   static async getMoney(){
+//     const defaultMoney=1000;
+//     setInterval(updateMoney, 6000);
+//     async function updateMoney() {
+
+//       var quiz = await DBConnector.getLatestQuiz()
+//       if(quiz.gotAnswer==0)// If no one send correct answer
+//       { 
+//         var quizTime = new Date(quiz.time)
+//         var nowTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"});
+//         nowTime = new Date(nowTime);
+//         var timeMoney=0
+//         if(nowTime.getDate()!=quizTime.getDate())
+//         {
+//           timeMoney=60*24
+//         }
+//         timeMoney=timeMoney+((nowTime.getHours() * 60  + nowTime.getMinutes() * 1 ) - (quizTime.getHours() * 60  + quizTime.getMinutes()*1))*2
+//         if(timeMoney<0)
+//         {
+//           timeMoney=0
+//         }
+//         var nowMoney=defaultMoney+timeMoney
+//         console.log(nowMoney)
+//         WSConnector.moneyBroadcast(nowMoney)
+//       }
+      
+//     }
+//   }
+
+  
+// }
 
