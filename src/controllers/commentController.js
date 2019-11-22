11715745eee2b0895246a62cb0968b10b75a527c
hchangjae <hcj@payw.org>
2019-11-22 21:39:53 +0900

    refactor: increase readability

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -4,15 +4,7 @@ const WSConnector = require('../websocket/WSConnector');
 exports.create = async (req, res) => {
     const axios = require('axios');
 
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var ip = req.headers['x-forwarded-for'] ||
-     req.connection.remoteAddress ||
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
-    var dateTime = date + ' ' + time;
-
+    // nickname
     var nickname = await DBConnector.getNickname(ip)
     if(nickname.length==0){
       const result = await axios.get('http://rng.api.quizlunch.com/new');
@@ -23,19 +15,45 @@ exports.create = async (req, res) => {
       nickname = nickname[0].nickname
     }
 
+    // ip
+    var ip = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+
+    // time
+    var today = new Date()
+    if(today.getHours() < 12)
+      today.setDate(today.getDate()+1)
+    var YYYY = today.getFullYear()
+    var MM = ('0'+(today.getMonth()+1)).slice(-2)
+    var DD = ('0'+(today.getDate())).slice(-2)
+    var YYYYMMDD = `${YYYY}-${MM}-${DD}`
+
     var comment = {
       'quizID':req.body.quizID,
       'nickname':nickname,
       'password':req.body.password,
       'text':req.body.text,
       'ip':ip,
-      'time':dateTime 
+      'time':YYYYMMDD 
     }
-    await DBConnector.insertComment(comment)
+    comment.commentID = await DBConnector.insertComment(comment)
+    delete comment.password;
     WSConnector.broadcast('insert comment', comment)
-    res.send(200)
+    res.sendStatus(200)
   };
 
+  exports.more = async (req, res) => {
+
+    console.log(req.query.quizID)
+    console.log(req.query.numOfComments)
+    await setTimeout(async ()=>{
+      const result = await DBConnector.getComments(req.query.quizID, req.query.numOfComments)
+      res.send(result)
+    },2000)
+
+  };
 
 exports.delete = async (req, res) => {
 
@@ -47,7 +65,7 @@ exports.delete = async (req, res) => {
         const comment = await DBConnector.getComments(req.body.quizID)  
         WSConnector.broadcast('delete comment', req.body.commentID)
     }
-    else 
+    else
     {
         res.send(200,'wrong password');
     }
