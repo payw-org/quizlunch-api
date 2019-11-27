b62bf172335182adf1c4056fb5814e232cf0b368
hchangjae <hcj@payw.org>
2019-11-27 17:32:37 +0900

    refactor: adopt change

--- src/websocket/WSConnector.js
+++ src/websocket/WSConnector.js
@@ -15,27 +15,11 @@ module.exports = class WSConnector {
             var result
 
             // get today quizID
-            var today = new Date()
-            if(today.getHours() < 12)
-                today.setDate(today.getDate()-1)
-            var YYYY = today.getFullYear()
-            var MM = ('0'+(today.getMonth()+1)).slice(-2)
-            var DD = ('0'+(today.getDate())).slice(-2)
-            var YYYYMMDD = `${YYYY}-${MM}-${DD}`
-            result = await DBQuizs.getIDByTime(YYYYMMDD)
+            result = await DBQuizs.getIDByTime()
             var todayQuizID =  result[0].quizID
 
             // get today quiz
             result = DBQuizs.getQuizByID(quizID)
-            if(result[0].money === 0){ // Not solved quiz
-                var quizStartAt = new Date()
-                if(quizStartAt.getHours() < 12)
-                    quizStartAt.setDay(quizStartAt.getDate()-1)
-                quizStartAt.setHours(12)
-                quizStartAt.setMinutes(0)
-                quizStartAt.setSeconds(0)
-                result[0].money = Math.floor((Date.now()-quizStartAt)/1000/20)
-            }
             data['renew quiz'] = result[0]
 
             // get today comments
